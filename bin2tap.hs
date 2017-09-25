9bdd8c2ba300e5849b1064c744e00a5a79195ed1
Daniel Bailey <danieljabailey@gmail.com>
2017-09-25 21:19:29 +0100

    Seems to do a thing, like what I want it to do

--- bin2tap.hs
+++ bin2tap.hs
@@ -1,5 +1,41 @@
 import System.Environment
 import Text.Read
+import Data.Int
+import Data.Word
+import Data.Bits
+import Data.Char
+import qualified Data.ByteString as BS
+import System.Posix
+
+getFileSize :: String -> IO FileOffset
+getFileSize path = do
+    stat <- getFileStatus path
+    return (fileSize stat)
+
+
+splitWord16_8 a = [(fromIntegral a :: Word8), (fromIntegral (shiftR a 8) :: Word8)]
+
+stringData :: String -> [Word8]
+stringData s = map (\c -> fromIntegral (ord c) :: Word8) s
+
+headerData :: Word8 -> String -> Word16 -> Word16 -> Word16 -> [Word8]
+headerData datatype name length ext1 ext2 =
+    [datatype] ++ (stringData name) ++ (splitWord16_8 length) ++ (splitWord16_8 ext1) ++ (splitWord16_8 ext2)
+
+checkSum :: [Word8] -> Word8
+checkSum blockData = foldl xor 0 blockData
+
+block :: Word8 -> [Word8] -> BS.ByteString
+block flag blockData = BS.pack ([flag] ++ blockData ++ [checkSum blockData])
+
+headerBlock blockData = block 0 blockData
+dataBlock blockData = block 255 blockData
+
+codeHeader :: String -> Word16 -> Word16 -> [Word8]
+codeHeader name length startaddr = headerData 3 name length startaddr 0
+
+codeHeaderBlock :: String -> Word16 -> Word16 -> BS.ByteString
+codeHeaderBlock name length startaddr = headerBlock (codeHeader name length startaddr)
 
 rangedAddr :: Int -> (Maybe Int)
 rangedAddr addr =
@@ -32,7 +68,8 @@ tapeConversion :: (Maybe Int) -> (Maybe String) -> String -> IO ()
 tapeConversion _addr _name filename = do
     addr <- maybe invalidAddr return _addr;
     name <- maybe invalidName return _name;
-    print outfilename;
+    filelen <- getFileSize filename;
+    BS.writeFile outfilename (codeHeaderBlock name (fromIntegral filelen :: Word16) (fromIntegral addr :: Word16))
     where outfilename = filename ++ ".tap"
 
 main = getArgs >>= maybe invalidArgs (\(addr, name, file) -> tapeConversion addr name file) . parseArgs
