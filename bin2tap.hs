643ad03a7d878ad021e43c10fc3086c0c3a5596c
= <nvd1234@gmail.com>
2017-09-26 05:54:10 +0100

    Introduce top-level and remove local type signatures

--- bin2tap.hs
+++ bin2tap.hs
@@ -13,12 +13,13 @@ getFileSize path = do
     stat <- getFileStatus path
     return (fileSize stat)
 
--- Split an unsigned 16 bit int into a pair of unsigned 8 bit ints
-splitWord16_8 a = [fromIntegral a :: Word8, fromIntegral (shiftR a 8) :: Word8]
+-- Split an unsigned 16 bit int into a pair) of unsigned 8 bit ints
+splitWord16_8 :: Word16 -> [Word8]
+splitWord16_8 a = [fromIntegral a, fromIntegral (shiftR a 8)]
 
 -- String to Word8 array
 stringData :: String -> [Word8]
-stringData s = map (\c -> fromIntegral (ord c) :: Word8) s
+stringData s = map (\c -> fromIntegral (ord c)) s
 
 -- For building the spectrum format header block data
 headerData :: Word8 -> String -> Word16 -> Word16 -> Word16 -> [Word8]
@@ -33,9 +34,10 @@ checkSum blockData = foldl xor 0 blockData
 -- the .tap format length field
 block :: Word8 -> [Word8] -> BS.ByteString
 block flag blockData = let payload = [flag] ++ blockData ++ [checkSum ([flag] ++ blockData)] in
-    BS.pack (splitWord16_8 (fromIntegral (length payload) :: Word16) ++ payload)
+    BS.pack (splitWord16_8 (fromIntegral (length payload)) ++ payload)
 
 -- helpers for the different types of blocks
+headerBlock, dataBlock :: [Word8] -> BS.ByteString
 headerBlock blockData = block 0 blockData
 dataBlock blockData = block 255 blockData
 
@@ -55,7 +57,7 @@ rangedAddr addr =
 
 -- for parsing address arg
 parseAddr :: String -> Maybe Int
-parseAddr addr = maybe Nothing rangedAddr (readMaybe addr :: Maybe Int)
+parseAddr addr = maybe Nothing rangedAddr (readMaybe addr)
 
 -- for parsing name arg (must be <= 10 length)
 parseName :: String -> Maybe String
@@ -69,13 +71,16 @@ parseArgs args = let l = length args in
     if l /= 3 then Nothing
     else Just (parseAddr (args !! 0), parseName (args !! 1), args !! 2)
 
+invalidArgs :: a
 invalidArgs = error ("Usage: bin2tap addr name file" ++
     "    where addr is the start address," ++
     "    name is the tape file name" ++
     "    and file is the local filename")
 
+invalidName :: a
 invalidName = error "Tape file name must be no more than 10 characters"
 
+invalidAddr :: a
 invalidAddr = error "Address must be an integer in the range 0 to 65535 (0x0000 to 0xFFFF)"
 
 -- Main conversion function
@@ -92,11 +97,12 @@ tapeConversion _addr _name filename = do
     -- save a new file that contains...
     BS.writeFile outfilename (
         -- ...a header block for the binary data...
-        codeHeaderBlock name (fromIntegral filelen :: Word16) (fromIntegral addr :: Word16)
+        codeHeaderBlock name (fromIntegral filelen) (fromIntegral addr)
         `BS.append`
         -- ...and the binary data block itself.
         dataBlock (BS.unpack filedata)
         )
     where outfilename = filename ++ ".tap"
 
+main :: IO ()
 main = getArgs >>= maybe invalidArgs (\(addr, name, file) -> tapeConversion addr name file) . parseArgs
